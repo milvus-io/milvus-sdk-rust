bf0900d41674c7d628e6d6094b94a9da408e9d26
yah <yang.cen@zilliz.com>
2022-07-09 23:24:48 +0800

    Fix CI for unit tests
    Signed-off-by: yah <yang.cen@zilliz.com>

--- build.rs
+++ build.rs
@@ -1,11 +1,13 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().out_dir("src/proto").compile(
+    tonic_build::configure()
+    .build_server(false)
+    .out_dir("src/proto").compile(
         &[
-            "./milvus-proto/proto/common.proto",
-            "./milvus-proto/proto/milvus.proto",
-            "./milvus-proto/proto/schema.proto",
+            "milvus-proto/proto/common.proto",
+            "milvus-proto/proto/milvus.proto",
+            "milvus-proto/proto/schema.proto",
         ],
-        &["./milvus-proto/proto"],
+        &["milvus-proto/proto"],
     )?;
 
     Ok(())
