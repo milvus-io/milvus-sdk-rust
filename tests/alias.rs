19bf7e5ff054003285771614df095a9a9ad73fcf
kingzcheung <kingzcheung@gmail.com>
2023-05-16 17:41:04 +0800

    Add collection alias api
    Signed-off-by: kingzcheung <kingzcheung@gmail.com>

---
+++ tests/alias.rs
@@ -0,0 +1,30 @@
+mod common;
+use common::*;
+use milvus::{collection::Collection, error::Result};
+
+async fn clean_test_collection(collection: Collection) -> Result<()> {
+    collection.drop().await?;
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_create_alias() -> Result<()> {
+    let collection = create_test_collection().await?;
+    collection.create_alias("test_alias").await?;
+    collection.drop_alias("test_alias").await?;
+    clean_test_collection(collection).await?;
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_alter_alias() -> Result<()> {
+    let alias = "test_alias";
+    let collection1 = create_test_collection().await?;
+    collection1.create_alias(alias).await?;
+    let collection2 = create_test_collection().await?;
+    collection2.alter_alias(alias).await?;
+    collection2.drop_alias(alias).await?;
+    clean_test_collection(collection1).await?;
+    clean_test_collection(collection2).await?;
+    Ok(())
+}
